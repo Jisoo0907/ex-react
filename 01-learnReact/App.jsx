020870e8de0e682de8e0b18fa4228e3e0638bdc9
jisoolee <szjslee@gmail.com>
2024-08-06 20:13:41 +0900

    learn React JSX 데이터 끼워넣기

--- 01-learnReact/App.jsx
+++ 01-learnReact/App.jsx
@@ -1,7 +1,20 @@
 function App() {
+  const abbr = {
+    apple: '초록사과',
+    pineapple: '노란파인애플',
+    text: {
+      color: '노란',
+      day: '8/6',
+    },
+  };
   return (
     <div className="App">
-      <h1>Vite + React Scaffolding</h1>
+      <h1>
+        <abbr title={' '}>사과</abbr>
+      </h1>
+      <h1>
+        <abbr title={abbr.pineapple}>파인애플</abbr>
+      </h1>
     </div>
   );
 }
