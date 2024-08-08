9c733cf3ced4c4b2b5eece03c507a126d9ad6044
jisoolee <szjslee@gmail.com>
2024-08-08 17:34:19 +0900

    degit 12번 폴더

---
+++ 03-learnReact/App.jsx
@@ -0,0 +1,16 @@
+function App(props) {
+  return (
+    <>
+      <h1>주의하세요</h1>
+    </>
+  );
+}
+export default App;
+
+export function Render() {
+  return (
+    <>
+      <App color={'pink'} />
+    </>
+  );
+}
