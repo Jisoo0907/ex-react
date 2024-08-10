cecb44e1593333a368014a9fc97e60a695124b72
jisoolee <szjslee@gmail.com>
2024-08-10 19:46:15 +0900

    STEP7 - 게임 승리자가 있는 지 확인하는 함수 작성, 승리자 정보 확인

--- 13-tic-tac-toe-game/src/tic-tac-toe/Components/Square/Square.jsx
+++ 13-tic-tac-toe-game/src/tic-tac-toe/Components/Square/Square.jsx
@@ -1,18 +1,24 @@
-import { func, node } from 'prop-types';
+import { func, node, object } from 'prop-types';
 import S from './Square.module.css';
 
 Square.propTypes = {
   children: node,
   onPlay: func,
+  style: object,
 };
 
 // 상태를 가지지 않는(Stateless) 컴포넌트
-function Square({ children, onPlay }) {
+function Square({ children, onPlay, style }) {
   // [파생된 상태]
   const isDisabled = !!children;
 
   return (
-    <button className={S.component} onClick={onPlay} disabled={isDisabled}>
+    <button
+      className={S.component}
+      style={style}
+      onClick={onPlay}
+      disabled={isDisabled}
+    >
       {children}
     </button>
   );
--- 13-tic-tac-toe-game/src/tic-tac-toe/Components/Squares/Squares.jsx
+++ 13-tic-tac-toe-game/src/tic-tac-toe/Components/Squares/Squares.jsx
@@ -4,59 +4,54 @@ import {
   PLAYER,
   PLAYER_COUNT,
   checkWinner,
+  WINNERS_COLOR,
 } from '@/tic-tac-toe/constants';
 import S from './Squares.module.css';
 import Square from '../Square/Square';
 
-// 상태를 가지는(Stateful) 컴포넌트
 function Squares() {
-  // [게임 상태] --------------------------------------------------------------
-
-  // 게임판(9개의 말판) 상태를 나타내는 리액트의 상태 선언
   const [squares, setSquares] = useState(INITIAL_SQUARES);
 
-  // [게임 상태 업데이트 기능] ----------------------------------------------------
-
-  // 게임을 진행하는 함수
-  const handlePlayGame = (index) => {
-    const result = checkWinner('????');
-    console.log({ result });
+  const handlePlayGame = (index) => () => {
     setSquares((prevSquares) => {
-      // 다음 번 컴포넌트 렌더링 시, 전달(계산)된 현재 시점의 상태: 이전 스퀘어 집합을 순환해서
-      const nextSquares = prevSquares.map((square, squareIndex) => {
-        // 개별 스퀘어의 인덱스와 사용자 행동에 따라 선택된 인덱스를 비교한다.
-        // 만약 그 값이 동일하다면?
-        if (squareIndex === index) {
-          return currentPlayer;
-        }
-        // 동일하지 않은 경우 그냥 이전 값을 반환한다.
-        return square;
+      const nextSquares = prevSquares.map((square, idx) => {
+        return idx === index ? currentPlayer : square;
       });
 
-      // 반환한 값이 다음 번 렌더링에서의 (스냅샷) 상태 값
       return nextSquares;
     });
   };
 
-  // [게임 파생된 상태] ----------------------------------------------------------
+  const winnerInfo = checkWinner(squares);
+  console.log('승자는?', winnerInfo);
 
-  // 게임 순서 (0, 1, 2, 3, ...)
-  const gameIndex = squares.filter(Boolean).length;
+  const gameIndex = squares.filter(Boolean).length; // 0
 
-  // 현재 게임 플레이어 ([0] PLAYER.ONE ↔ [1] PLAYER.TWO)
-  // 첫번째 플레이어의 턴인가요?
-  const isPlayerOneTurn = gameIndex % PLAYER_COUNT === 0;
-  // 첫번째 플레이어의 턴이면 PLAYER.ONE 아니면 PLAYER.TWO
-  const currentPlayer = isPlayerOneTurn ? PLAYER.ONE : PLAYER.TWO;
+  const isPlayerOneTurn = gameIndex % PLAYER_COUNT === 0; // true
 
-  console.log('렌더링\n\n', { squares, gameIndex, currentPlayer });
+  const currentPlayer = isPlayerOneTurn ? PLAYER.ONE : PLAYER.TWO; // '🍟'
 
   return (
     <div className={S.component}>
       {/* 리액트 (JSX) 마크업 : 리스트 렌더링 */}
       {squares.map((square, index) => {
+        const winnerStyles = {
+          backgroundColor: null,
+        };
+
+        if (winnerInfo) {
+          const [x, y, z] = winnerInfo.condition;
+          if (index === x || index === y || index === z) {
+            winnerStyles.backgroundColor = WINNERS_COLOR;
+          }
+        }
+
         return (
-          <Square key={index} onPlay={() => handlePlayGame(index)}>
+          <Square
+            key={index}
+            style={winnerStyles}
+            onPlay={handlePlayGame(index)}
+          >
             {square}
           </Square>
         );
