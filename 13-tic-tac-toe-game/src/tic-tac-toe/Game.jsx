fded6f7831a394d2e56c9c62f25323b03550af72
jisoolee <szjslee@gmail.com>
2024-08-10 10:19:22 +0900

    STEP1 - 컴포넌트 추출, 컴포넌트 트리 구성

--- 13-tic-tac-toe-game/src/tic-tac-toe/Game.jsx
+++ 13-tic-tac-toe-game/src/tic-tac-toe/Game.jsx
@@ -1,33 +1,12 @@
+import Board from './Components/Board/Board';
+import { PLAYER } from './constants';
+import History from './Components/History/History';
+
 function Game() {
   return (
     <div className="Game">
-      <div className="Board">
-        <h2 className="Status">플레이어 : 🟨</h2>
-        {/* <h2>승자! : 🟨</h2> */}
-        {/* <h2>비겼어요... 😳</h2> */}
-        <div className="Squares">
-          <button className="Square" disabled>
-            🟨
-          </button>
-          <button className="Square"></button>
-          <button className="Square"></button>
-          <button className="Square"></button>
-          <button className="Square" disabled>
-            ⚫️
-          </button>
-          <button className="Square"></button>
-          <button className="Square"></button>
-          <button className="Square"></button>
-          <button className="Square"></button>
-        </div>
-      </div>
-      <div className="History">
-        <ol>
-          <li>
-            <button type="button">게임 시작</button>
-          </li>
-        </ol>
-      </div>
+      <Board />
+      <History />
     </div>
   );
 }
