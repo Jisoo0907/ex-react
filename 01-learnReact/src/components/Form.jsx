b4a219615c5f68836b8835dfd81b0b3cc38d7c37
jisoolee <szjslee@gmail.com>
2024-08-24 11:05:04 +0900

    form useState

---
+++ 01-learnReact/src/components/Form.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+function Form() {
+  const [first, setFirst] = useState('');
+  const [last, setLast] = useState('');
+
+  function handleFirstNameChange(e) {
+    setFirst(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLast(e.target.value);
+  }
+
+  function handleReset() {
+    setFirst('');
+    setLast('');
+  }
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <input
+        placeholder="First name"
+        value={first}
+        onChange={handleFirstNameChange}
+      />
+      <input
+        placeholder="Last name"
+        value={last}
+        onChange={handleLastNameChange}
+      />
+      <h1>
+        Hi, {first} {last}
+      </h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
+  );
+}
+export default Form;
--- 01-learnReact/src/main.jsx
+++ 01-learnReact/src/main.jsx
@@ -3,11 +3,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '/App';
 import Data from './components/data';
+import Form from './components/Form';
 
 const domNode = document.getElementById('react-app');
 
 createRoot(domNode).render(
   <StrictMode>
-    <Data />
+    <Form />
   </StrictMode>
 );
